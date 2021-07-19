d0665aa6161bb1ca68699a6be1342aab93383509
tanyamelnyk19 <den.tani19@gmail.com>
2021-07-20 02:19:55 +0300

    fixed

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,10 +3,7 @@ console.log(`В списке ${categoryItems.length} категории.`);
 
 const categoryItemContent = [...categoryItems].map(categoryItem => {
     const categoryItemTitle = categoryItem.querySelector('h2');
-    // console.log('categoryItemTitle :>> ', categoryItemTitle);
     const categoryItemEL = categoryItem.querySelectorAll('li');
-    // console.log('categoryItemEL :>> ', categoryItemEL);
     return `Категория: ${categoryItemTitle.textContent}, Количество элементов: ${categoryItemEL.length}; `;
-
 }).join('');
 console.log(categoryItemContent);
